9368253b284d0c3ff692619f7458c37087b0d1eb
Alex Han <lx.han@ya.ru>
2026-04-30 16:45:02 +0900

    Initial Commit

---
+++ MightyMittsX/MightyMittsXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MightyMittsXApp.swift
+//  MightyMittsX
+//
+//  Created by Alex Han on 2026.04.30.
+//
+
+import SwiftUI
+
+@main
+struct MightyMittsXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
